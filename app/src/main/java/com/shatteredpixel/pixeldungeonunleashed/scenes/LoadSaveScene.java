2778d952ca5970a45fe55d7e5059f9f389340e63
badcw <15549473954@163.com>
2017-08-08 12:46:04 +0800

    initial

---
+++ app/src/main/java/com/shatteredpixel/pixeldungeonunleashed/scenes/LoadSaveScene.java
@@ -0,0 +1,420 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015  Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2015 Evan Debenham
+ *
+ * Unleashed Pixel Dungeon
+ * Copyright (C) 2015 David Mitchell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+// NOTE - the idea behind Load/Save functionality was originated in Soft Pixel Dungeon
+package com.shatteredpixel.pixeldungeonunleashed.scenes;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import com.shatteredpixel.pixeldungeonunleashed.Dungeon;
+import com.shatteredpixel.pixeldungeonunleashed.GamesInProgress;
+import com.shatteredpixel.pixeldungeonunleashed.levels.Terrain;
+import com.shatteredpixel.pixeldungeonunleashed.ui.Archs;
+import com.shatteredpixel.pixeldungeonunleashed.ui.RedButton;
+import com.shatteredpixel.pixeldungeonunleashed.ui.Window;
+import com.shatteredpixel.pixeldungeonunleashed.utils.Utils;
+import com.shatteredpixel.pixeldungeonunleashed.windows.WndOptions;
+import com.shatteredpixel.pixeldungeonunleashed.windows.WndStory;
+import com.watabou.noosa.BitmapText;
+import com.watabou.noosa.Camera;
+import com.watabou.noosa.Game;
+import com.watabou.utils.Bundle;
+
+import android.annotation.SuppressLint;
+
+@SuppressLint("DefaultLocale")
+public class LoadSaveScene extends PixelScene {
+
+    private static final float BUTTON1_WIDTH = 34;
+    private static final float BUTTON2_WIDTH = 55;
+    private static final float BUTTON_HEIGHT = 20;
+    private static final float BUTTON_PADDING = 3;
+
+    private static final String TXT_TITLE	= "Save/Load ";
+
+    private static final String TXT_LOAD	= "Load";
+    private static final String TXT_SAVE	= "Save";
+    private static final String TXT_SLOTNAME= "Game";
+
+    private static final String HERO		= "hero";
+    private static final String DEPTH		= "depth";
+    private static final String DIFLEV      = "diflev";
+    private static final String LEVEL		= "lvl";
+
+    private static final String TXT_REALLY	= "Load";
+    private static final String TXT_WARNING	= "Your current progress will be lost and Difficulty Levels may be changed.";
+    private static final String TXT_YES		= "Yes, load " + TXT_SLOTNAME;
+    private static final String TXT_NO		= "No, return to main menu";
+
+    private static final String TXT_DPTH_LVL	= "Depth: %d, level: %d";
+
+    private static final int CLR_WHITE	= 0xFFFFFF;
+
+
+    @Override
+    public void create() {
+
+        super.create();
+
+        uiCamera.visible = false;
+
+        int w = Camera.main.width;
+        int h = Camera.main.height;
+
+        Archs archs = new Archs();
+        archs.setSize( w, h );
+        add( archs );
+
+        String showClassName = capitalizeWord(Dungeon.hero.heroClass.title());
+
+
+        String diffLevel;
+        String saveInstructions;
+        switch (Dungeon.difficultyLevel) {
+            case Dungeon.DIFF_TUTOR:
+                diffLevel = "TUTORIAL";
+                saveInstructions = "In Tutorial mode you may save anywhere.";
+                break;
+            case Dungeon.DIFF_EASY:
+                diffLevel = "EASY";
+                saveInstructions = "In Easy mode you may save anywhere.";
+                break;
+            case Dungeon.DIFF_HARD:
+                diffLevel = "HARD";
+                saveInstructions = "In Hard mode you may save after a boss level.";
+                break;
+            case Dungeon.DIFF_NTMARE:
+                diffLevel = "NIGHTMARE";
+                saveInstructions = "Games may not be saved in Nightmare mode.";
+                break;
+            case Dungeon.DIFF_ENDLESS:
+                diffLevel = "ENDLESS";
+                saveInstructions = "Games may not be saved in Endless mode.";
+                break;
+            case Dungeon.DIFF_TEST:
+                diffLevel = "TEST";
+                saveInstructions = "In Test mode you may save anywhere.";
+                break;
+            default:
+                diffLevel = "NORMAL";
+                saveInstructions = "In Normal mode you may save at level entrance signs.";
+                break;
+        }
+        BitmapText title = PixelScene.createText( TXT_TITLE + showClassName + " - " + diffLevel, 9, true );
+        title.hardlight( Window.TITLE_COLOR );
+        title.measure();
+        title.x = align( (w - title.width()) / 2 );
+        title.y = BUTTON_PADDING;
+        add( title );
+
+        String currentProgress = "dead";
+        GamesInProgress.Info info = GamesInProgress.check(Dungeon.hero.heroClass);
+        if (info != null) {
+            currentProgress = Utils.format(TXT_DPTH_LVL, info.depth, info.level);
+        }
+        BitmapText subTitle = PixelScene.createText( "Currently " + currentProgress, 6, false );
+        subTitle.hardlight(Window.TITLE_COLOR);
+        subTitle.measure();
+        subTitle.x = align( (w - title.width()) / 2 );
+        subTitle.y =  (BUTTON_HEIGHT / 2) + BUTTON_PADDING ;
+        add(subTitle);
+
+        BitmapText saveInfo = PixelScene.createText( saveInstructions, 6, false );
+        saveInfo.hardlight( Window.TITLE_COLOR );
+        saveInfo.measure();
+        saveInfo.x = align( (w - saveInfo.width()) / 2 );
+        saveInfo.y = BUTTON_PADDING + BUTTON_HEIGHT;
+        add( saveInfo );
+
+
+
+        int posY = (int) (BUTTON_HEIGHT + (BUTTON_PADDING * 3.5f));
+        int posX2 = w - (int) (BUTTON2_WIDTH + BUTTON_PADDING);
+        int posX = (int) (BUTTON1_WIDTH + (BUTTON_PADDING * 3));
+
+        String[] slotList = { "A", "B", "C", "D", "E" };
+
+        String classInfo = Dungeon.hero.heroClass.title();
+
+        for (String saveSlot : slotList) {
+            // add the row caption..
+            BitmapText buttonCapton1 = PixelScene.createText(TXT_SLOTNAME + " " + saveSlot, 9, true);
+            buttonCapton1.hardlight(CLR_WHITE);
+            buttonCapton1.measure();
+            buttonCapton1.x = BUTTON_PADDING;
+            buttonCapton1.y = posY + (BUTTON_HEIGHT / 3);
+            add(buttonCapton1);
+
+            // add the save button..
+            if (Dungeon.hero.isAlive() &&
+                    (Dungeon.difficultyLevel <= Dungeon.DIFF_EASY) || (Dungeon.difficultyLevel <= Dungeon.DIFF_TEST) ||
+                    (Dungeon.difficultyLevel == Dungeon.DIFF_NORM &&
+                            Dungeon.level.isAdjacentTo(Dungeon.hero.pos, Terrain.SIGN)) ||
+                    (Dungeon.difficultyLevel == Dungeon.DIFF_HARD &&
+                            Dungeon.level.isAdjacentTo(Dungeon.hero.pos, Terrain.SIGN) &&
+                            Dungeon.bossLevel(Dungeon.depth - 1))) {
+
+                GameButton btnSave = new GameButton(this, true, TXT_SAVE, "", classInfo, saveSlot);
+                add(btnSave);
+                btnSave.visible = true;
+                btnSave.setRect(posX, posY, BUTTON1_WIDTH, BUTTON_HEIGHT);
+            }
+
+            // add the load button if there are saved files to load..
+            String saveSlotFolder = Game.instance.getFilesDir().toString() + "/" + classInfo + saveSlot;
+
+            File backupFolder = new File(saveSlotFolder);
+            if (backupFolder.exists()) {
+                FileInputStream input;
+                try {
+                    input = new FileInputStream(saveSlotFolder + "/" + classInfo + ".dat");
+                    Bundle bundle = Bundle.read(input);
+                    input.close();
+                    int savedDepth = bundle.getInt(DEPTH, 1);
+                    Bundle savedHero = bundle.getBundle(HERO);
+                    int savedDif = bundle.getInt(DIFLEV);
+                    int savedLevel = savedHero.getInt(LEVEL);
+                    String savedProgress = Utils.format(TXT_DPTH_LVL, savedDepth, savedLevel);
+                    String loadLevel;
+                    switch (savedDif) {
+                        case Dungeon.DIFF_TUTOR:
+                            loadLevel = TXT_LOAD + " TUTOR";
+                            break;
+                        case Dungeon.DIFF_EASY:
+                            loadLevel = TXT_LOAD + " EASY";
+                            break;
+                        case Dungeon.DIFF_HARD:
+                            loadLevel = TXT_LOAD + " HARD";
+                            break;
+                        case Dungeon.DIFF_NTMARE:
+                            loadLevel = TXT_LOAD + " NTMARE";
+                            break;
+                        case Dungeon.DIFF_TEST:
+                            loadLevel = TXT_LOAD + " TEST";
+                            break;
+                        case Dungeon.DIFF_ENDLESS:
+                            loadLevel = TXT_LOAD + " ENDLESS";
+                            break;
+                        default:
+                            loadLevel = TXT_LOAD + " NORMAL";
+                            break;
+                    }
+                    GameButton btnLoad1A = new GameButton(this, false, loadLevel, savedProgress, classInfo, saveSlot);
+
+                    add(btnLoad1A);
+                    btnLoad1A.visible = true;
+                    btnLoad1A.setRect(posX2, posY, (int) (BUTTON2_WIDTH), BUTTON_HEIGHT);
+                } catch (FileNotFoundException e) {
+                    //e.printStackTrace();
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                } catch (NullPointerException e) {
+                    //e.printStackTrace();
+                }
+            }
+            // move down the line now...
+            posY += BUTTON_HEIGHT + BUTTON_PADDING;
+        }
+
+
+        fadeIn();
+    }
+
+    @Override
+    protected void onBackPressed() {
+        InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
+        Game.switchScene( InterlevelScene.class );
+    }
+
+    protected static void exportGames(String classInfo, String saveSlot) {
+        ArrayList<String> files = new ArrayList<>();
+        String saveSlotFolder = Game.instance.getFilesDir().toString() + "/" + classInfo + saveSlot;
+        makeFolder(saveSlotFolder);
+
+        for(String fileName : Game.instance.fileList()){
+            if(isGameLevelFile(classInfo, fileName)){
+                files.add(fileName);
+            }
+        }
+
+        // remove previous saved game files..
+        File backupFolder = new File(saveSlotFolder);
+
+        for(File backupFile : backupFolder.listFiles()){
+            if(isGameLevelFile(classInfo, backupFile.getName())){
+                backupFile.delete();
+            }
+        }
+
+        for (String fileName : files){
+            try {
+                FileInputStream in = Game.instance.openFileInput(fileName);
+                OutputStream out = new FileOutputStream(saveSlotFolder + "/" + fileName);
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                WndStory.showChapter("Failed to save file " + fileName);
+                //Log.d("FAILED EXPORT", f);
+            }
+        }
+        InterlevelScene.mode = InterlevelScene.Mode.SAVE;
+        Game.switchScene( InterlevelScene.class );
+    }
+
+    private static boolean isGameLevelFile(String classInfo, String fileName) {
+        return fileName.endsWith(".dat") && (fileName.startsWith(classInfo));
+    }
+
+    private static void makeFolder(String saveSlotFolder) {
+        File dir = new File(saveSlotFolder);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+    }
+
+    protected static void importGames(String classInfo, String saveSlot) {
+        ArrayList<String> files = new ArrayList<>();
+        String saveSlotFolder = Game.instance.getFilesDir().toString() + "/" + classInfo + saveSlot;
+        File backupFolder = new File(saveSlotFolder);
+
+        for(File backupFile : backupFolder.listFiles()){
+            if(isGameLevelFile(classInfo, backupFile.getName())){
+                files.add(backupFile.getName());
+            }
+        }
+
+        // remove in progress game files..
+        for(String fileName : Game.instance.fileList()){
+            if(fileName.startsWith("game_") || isGameLevelFile(classInfo, fileName)){
+                Game.instance.deleteFile(fileName);
+            }
+        }
+
+
+        for (String fileName : files){
+            try {
+                FileInputStream in = new FileInputStream(saveSlotFolder + "/" + fileName); //
+                OutputStream out = Game.instance.openFileOutput(fileName, Game.MODE_PRIVATE );
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                WndStory.showChapter("Failed to load file " + fileName);
+            }
+        }
+        InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
+        Game.switchScene( InterlevelScene.class );
+    }
+
+    private static class GameButton extends RedButton {
+
+        private static final int SECONDARY_COLOR	= 0xCACFC2;
+
+        private BitmapText secondary;
+        private Boolean isSave = true;
+        private String classInfo = "";
+        private String saveSlot = "";
+        private LoadSaveScene loadSaveScene;
+
+        public GameButton(LoadSaveScene loadSaveScene, Boolean isSave, String primary, String secondary, String classInfo, String saveSlot ) {
+            super( primary );
+            this.secondary( secondary );
+            this.isSave = isSave;
+            this.classInfo = classInfo;
+            this.saveSlot = saveSlot;
+            this.loadSaveScene = loadSaveScene;
+        }
+        @Override
+        protected void onClick() {
+            if (isSave) {
+                exportGames(classInfo, saveSlot);
+            } else {
+                loadSaveScene.add( new WndOptions( TXT_REALLY + " " +saveSlot + " " + secondary.text() + "?", TXT_WARNING, TXT_YES + " " + saveSlot, TXT_NO ) {
+                    @Override
+                    protected void onSelect( int index ) {
+                        if (index == 0) {
+                            importGames(classInfo, saveSlot);
+                        }
+                    }
+                } );
+            }
+        }
+
+        @Override
+        protected void createChildren() {
+            super.createChildren();
+
+            secondary = createText( 6, false );
+            secondary.hardlight( SECONDARY_COLOR );
+            add( secondary );
+        }
+
+        @Override
+        protected void layout() {
+            super.layout();
+
+            if (secondary.text().length() > 0) {
+                text.y = y + (height - text.height() - secondary.baseLine()) / 2;
+
+                secondary.x = align( x + (width - secondary.width()) / 2 );
+                secondary.y = align( text.y + text.height() );
+            } else {
+                text.y = y + (height - text.baseLine()) / 2;
+            }
+        }
+
+        public void secondary( String text ) {
+            secondary.text( text );
+            secondary.measure();
+        }
+
+    }
+    public static String capitalizeWord(String oneWord)
+    {
+        return Character.toUpperCase(oneWord.charAt(0)) + oneWord.substring(1);
+    }
+}
---
+++ app/src/main/java/com/shatteredpixel/pixeldungeonunleashed/scenes/WelcomeScene.java
@@ -0,0 +1,179 @@
+/*
+ * Unleashed Pixel Dungeon
+ * Copyright (C) 2015  David Mitchell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.shatteredpixel.pixeldungeonunleashed.scenes;
+
+import com.shatteredpixel.pixeldungeonunleashed.Chrome;
+import com.shatteredpixel.pixeldungeonunleashed.GoblinsPixelDungeon;
+import com.shatteredpixel.pixeldungeonunleashed.ui.Archs;
+import com.shatteredpixel.pixeldungeonunleashed.ui.RedButton;
+import com.shatteredpixel.pixeldungeonunleashed.ui.ScrollPane;
+import com.shatteredpixel.pixeldungeonunleashed.ui.Window;
+import com.shatteredpixel.pixeldungeonunleashed.windows.WndMessage;
+import com.watabou.noosa.BitmapTextMultiline;
+import com.watabou.noosa.Camera;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.NinePatch;
+import com.watabou.noosa.ui.Component;
+
+//TODO: update this class with relevant info as new versions come out.
+public class WelcomeScene extends PixelScene {
+
+	private static final String TTL_Welcome = "Welcome!";
+
+	private static final String TTL_Update = "v0.1.5.2: Release";
+
+	private static final String TTL_Future = "Wait What?";
+
+	private static final String TXT_Welcome =
+			"Goblins Pixel Dungeon is a rework/expansion of Unleashed Pixel Dungeon.\n\n"+
+			"The game is a work in progress, so bear with me in case of bugs. :3\n"+
+			"To change difficulty levels click on the Out-of-Game Settings (little gears) in "+
+			"the top left corner, difficulty will also change when you load games.\n\n"+
+			"Enjoy";
+
+	private static final String TXT_Update =
+			"v0.1.5.2: Release\n"+
+					"- New font only for big texts.\n"+
+					"- Fixed SoW blast freeze.\n"+
+					"- Klik equipped item feeding fixed.\n"+
+					"- Klik vs. Mr. Fingers crash fix.\n"+
+			"v0.1.5.1: Release\n"+
+					"- Bugfix for Klik vulnerabilities.\n"+
+					"- Bugfix for phase shift Klik.\n"+
+					"- Reworked lock generation.\n"+
+					"- Fixed lock picking in hell.\n"+
+					"- Added resist and immune sprite info.\n"+
+                    "- Slightly nerfed wolves.\n"+
+					"- Bugfixed message log flooding.\n"+
+					"- Changed font.\n"+
+			"v0.1.5: Release\n"+
+					"- Added Lock Picking. :)\n"+
+					"- Added new Artifact: Hummingtool\n"+
+					"- Added Easter Eggs to be hatched.\n"+
+					"- Nerfed Metal Klik.\n"+
+					"- Revamped Klik immunities.\n"+
+					"- Added Klik vulnerabilities.\n"+
+					"- Klik damage declines with health.\n"+
+					"- Klik exp to level up increased.\n"+
+					"- Increased metal klik cooldown.\n";
+
+	private static final String TXT_Future =
+			"It seems that your current saves are from a future version of Goblins Pixel Dungeon!\n\n"+
+			"Either you're messing around with older versions of the app, or something has gone buggy.\n\n"+
+			"Regardless, tread with caution! Your saves may contain things which don't exist in this version, "+
+			"this could cause some very weird errors to occur.";
+
+	@Override
+	public void create() {
+		super.create();
+
+		final int gameversion = GoblinsPixelDungeon.version();
+
+		BitmapTextMultiline title;
+		BitmapTextMultiline text;
+
+		if (gameversion == 0) {
+
+			text = createMultiline(TXT_Welcome, 9, false);
+			title = createMultiline(TTL_Welcome, 14, true);
+
+		} else if (gameversion <= Game.versionCode) {
+
+			text = createMultiline(TXT_Update, 6, false );
+			title = createMultiline(TTL_Update, 9, true );
+
+		} else {
+
+			text = createMultiline( TXT_Future, 9, false );
+			title = createMultiline( TTL_Future, 14, true );
+
+		}
+
+		int w = Camera.main.width;
+		int h = Camera.main.height;
+
+		int pw = w - 10;
+		int ph = h - 50;
+
+		title.maxWidth = pw;
+		title.measure();
+		title.hardlight(Window.SHPX_COLOR);
+
+		title.x = align( (w - title.width()) / 2 );
+		title.y = align( 8 );
+		add( title );
+
+		NinePatch panel = Chrome.get(Chrome.Type.WINDOW);
+		panel.size( pw, ph );
+		panel.x = (w - pw) / 2;
+		panel.y = (h - ph) / 2;
+		add( panel );
+
+		ScrollPane list = new ScrollPane( new Component() );
+		add( list );
+		list.setRect(
+				panel.x + panel.marginLeft(),
+				panel.y + panel.marginTop(),
+				panel.innerWidth(),
+				panel.innerHeight());
+		list.scrollTo( 0, 0 );
+
+		Component content = list.content();
+		content.clear();
+
+		text.maxWidth = (int) panel.innerWidth();
+		text.measure();
+
+		content.add(text);
+
+		content.setSize( panel.innerWidth(), text.height() );
+
+		RedButton okay = new RedButton("Okay!") {
+			@Override
+			protected void onClick() {
+				GoblinsPixelDungeon.version(Game.versionCode);
+				Game.switchScene(TitleScene.class);
+			}
+		};
+
+		//okay.setRect(text.x, text.y + text.height() + 5, 55, 18);
+        okay.setRect(pw - 60, h - 22, 55, 18);
+		add(okay);
+
+		RedButton changes = new RedButton("Changes") {
+			@Override
+			protected void onClick() {
+				parent.add(new WndMessage(TXT_Update));
+			}
+		};
+
+		changes.setRect(((w - pw) / 2) + 10, h - 22, 55, 18);
+		add(changes);
+/*
+		okay.setRect((w - pw) / 2, h - 22, pw, 18);
+		add(okay);*/
+
+		Archs archs = new Archs();
+		archs.setSize( w, h );
+		addToBack( archs );
+
+		fadeIn();
+	}
+}
+
+
